d0ef7a3eff2c5eb145d920a7d53910f852633fcb
Amjad Menouer <menouer.amj@gmail.com>
2025-10-04 19:26:11 +0200

    to buy list next js main feature

---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="navbar bg-primary text-primary-content mb-8">
+            <button className="btn btn-ghost text-xl">Organizer App</button>
+        </header>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,92 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [array, setArray] = useState<any[]>([
+    {nom:"poires",quantite:3},
+    {nom:"pommes de terre",quantite:7},
+    {nom:"tomate",quantite:4},
+    {nom:"poireau",quantite:1}
+  ]);
+  const [erreur, setErreur] = useState<boolean>(false);
+  
+
+  function ajouterElement(){
+    let newValue = {
+      nom : (document.getElementById("elementAchatAjout") as HTMLInputElement).value,
+      quantite : (document.getElementById("elementQuantiteAjout") as HTMLInputElement).value
+    }
+
+    if(newValue.nom && newValue.quantite){
+      setErreur(false);
+      setArray([...array, newValue]);
+      (document.getElementById("elementAchatAjout") as HTMLInputElement).value = "";
+      (document.getElementById("elementQuantiteAjout") as HTMLInputElement).value = "";
+    }else{
+      setErreur(true);
+    }
+  }
+
+  function modifier(index: number){
+
+  }
+
+  function supprimer(index:number){
+    console.log("test")
+    setArray(array.slice(0,index).concat(array.slice(index+1)));
+  }
+
+  return (
+    <div>
+      <Header/>
+      <main className="flex flex-col justify-center-safe gap-[32px] row-start-2 items-center">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Nom</th>
+                <th>Quantité</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {array.map((item,index)=>
+                <tr key={index}>
+                  <th>{index+1}</th>
+                  <td>{item.nom}</td>
+                  <td>{item.quantite}</td>
+                  <td className="flex flex-row">
+                    <img onClick={() => modifier(index)} className="mr-4 hover:cursor-pointer" width="18" height="18" src="https://img.icons8.com/forma-regular/24/create-new.png" alt="create-new"/>
+                    <svg onClick={() => supprimer(index)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" strokeWidth="1.8" stroke="currentColor" className="size-[1.2em] hover:cursor-pointer">
+                      <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"></path>
+                    </svg>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
+        <div className="join ">
+          <input id="elementAchatAjout" type="text" className="join-item input input-primary w-stretch" placeholder="ajouter un truc a acheter"></input>
+          <input
+            id="elementQuantiteAjout"
+            type="number"
+            className="join-item input input-primary w-fit"
+            required
+            placeholder="1"
+            min="1"
+            max="99"
+          />
+          <button onClick={ajouterElement} className="join-item rounded-r-full btn btn-outline btn-primary">Ajouter</button>
+        </div>
+        {erreur && <p style={{ color: "red" }}>Veuillez entrer une valeur valide.</p>}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer/>
     </div>
   );
 }
