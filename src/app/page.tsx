2b244ed4192253c52a93e59f1b641bc40a02021a
Amjad Menouer <menouer.amj@gmail.com>
2025-10-04 19:41:55 +0200

    commenting edit icon button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
                   <td>{item.nom}</td>
                   <td>{item.quantite}</td>
                   <td className="flex flex-row">
-                    <img onClick={() => modifier(index)} className="mr-4 hover:cursor-pointer" width="18" height="18" src="https://img.icons8.com/forma-regular/24/create-new.png" alt="create-new"/>
+                    {/*<img onClick={() => modifier(index)} className="mr-4 hover:cursor-pointer" width="18" height="18" src="https://img.icons8.com/forma-regular/24/create-new.png" alt="create-new"/>*/}
                     <svg onClick={() => supprimer(index)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" strokeWidth="1.8" stroke="currentColor" className="size-[1.2em] hover:cursor-pointer">
                       <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"></path>
                     </svg>
